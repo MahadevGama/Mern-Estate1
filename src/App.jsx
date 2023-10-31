582bbdd31cce2cebad3e44c8b2df4973b7e82246
MahadevGama <mahadevgama21@gmail.com>
2023-10-31 17:13:39 +0530

    Creating Pages and Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import Home from './Pages/Home';
+import About from './Pages/About';
+import SignIn from './Pages/SignIn';
+import SignOut from './Pages/SignOut';
+import Profile from './Pages/Profile';
 
 function App() {
   return (
-    <div>
-      <h1 className='text-red-600'>Mahadev</h1>
-    </div>
+  <BrowserRouter>
+  <Routes>
+    <Route path='/' element={<Home/>}/>
+    <Route path='/profile' element={<Profile/>}/>
+    <Route path='/about' element={<About/>}/>
+    <Route path='/sign-in' element={<SignIn/>}/>
+    <Route path='/sign-out' element={<SignOut/>}/>
+    
+  </Routes>
+  </BrowserRouter>
   )
 }
 
